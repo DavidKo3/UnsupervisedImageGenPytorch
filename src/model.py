eec3c4e5497279b9f288756466f53032d6e4b72a
davidk <davidk@davidk-WA50SJQ>
2017-11-08 19:27:21 +0900

    model

---
+++ src/model.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def deconv(c_in, c_out, k_size, stride=2, pad=1, bn=True):
+    
+    layers = []
+    layers.append(nn.ConvTranspose2d(c_in, c_out, k_size, stride, pad, bias=False))
+    if bn:
+        layers.append(nn.BatchNorm2d(c_out))
+    return nn.Sequential(*layers)
+
+def conv(c_in, c_out, k_size, stride=2, pad=1, bn=True):
+    
+    layers=[]
+    layers.append(nn.Conv2d(c_in, c_out, k_size, stride, pad, bias=False))
+    if bn:
+        layers.append(nn.BatchNorm2d(c_out))
+    return nn.Sequential(*layers)
