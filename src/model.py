4528abfbde790f686dd68eb7bc41c430ffb6e091
davidk <davidk@davidk-WA50SJQ>
2017-11-09 10:22:14 +0900

    model imple

--- src/model.py
+++ src/model.py
@@ -18,3 +18,37 @@ def conv(c_in, c_out, k_size, stride=2, pad=1, bn=True):
         layers.append(nn.BatchNorm2d(c_out))
     return nn.Sequential(*layers)
 
+class D1(nn.Module):
+    """Discriminator for mnist."""
+    def __init__(self, conv_dim=64, use_labels=False):
+        super(D1, self).__init__()
+        self.conv1 = conv(1, conv_dim, 4, bn=False)
+        self.conv2 = conv(conv_dim, conv_dim*2, 4)
+        self.conv3 = conv(conv_dim*2, conv_dim*4, 4)
+        n_out = 11 if use_labels else 1
+
+    def forward(self, x):
+        out = F.relu(self.conv1(x), 0.05)   # (?, 64, 16, 16)
+        out = F.relu(self.conv2(x), 0.05)   # (?, 128, 8, 8)
+        out = F.relu(self.conv3(x), 0.05)   # (?, 256, 4, 4)
+        out = self.fc(out).squeeze()
+        return out
+    
+    
+class D2(nn.Module):
+    """Discriminator for svhn."""
+    def __init__(self, conv_dim=64, use_labels=False):
+        super(D2, self).__init__()
+        self.conv1 = conv(3, conv_dim, 4, bn=False)
+        self.conv2 = conv(conv_dim, conv_dim*2, 4)
+        self.conv3 = conv(conv_dim*2, conv_dim*4, 4)
+        n_out = 11 if use_labels else 1
+        self.fc = conv(conv_dim*4, n_out, 4, 1, 0, False)
+        
+    def forward(self, x):
+        out = F.leaky_relu(self.conv1(x), 0.05)    # (?, 64, 16, 16)
+        out = F.leaky_relu(self.conv2(out), 0.05)  # (?, 128, 8, 8)
+        out = F.leaky_relu(self.conv3(out), 0.05)  # (?, 256, 4, 4)
+        out = self.fc(out).squeeze()
+        return out
+    
